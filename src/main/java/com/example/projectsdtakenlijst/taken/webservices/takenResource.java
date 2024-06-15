8198c163af7db8848b1a1e0f06ae4899af0656cc
linde <linde.stam@student.hu.nl>
2024-06-15 16:59:46 +0200

    linde

--- src/main/java/com/example/projectsdtakenlijst/taken/webservices/takenResource.java
+++ src/main/java/com/example/projectsdtakenlijst/taken/webservices/takenResource.java
@@ -3,13 +3,12 @@ package com.example.projectsdtakenlijst.taken.webservices;
 import com.example.projectsdtakenlijst.taken.modules.Taak;
 import com.example.projectsdtakenlijst.taken.modules.alleTaken;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.AbstractMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Path("taken")  // Base URI
@@ -24,5 +23,32 @@ public class takenResource {
         }
         List<Taak> taken = taak.getTaken();
         return Response.ok(taken).build();
+    }
+
+    @GET
+    @Path("{afgevinkt}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAfgevinkteTaken(@PathParam("afgevinkt")alleTaken taak) {
+        alleTaken afgevinkteTaak = alleTaken.getTaak();
+        List<Taak> afgevinkteTaken = afgevinkteTaak.getAfgevinkteTaken();
+        return Response.ok(afgevinkteTaken).build();
+    }
+    @PUT
+    @Path("afgevinkt/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response voegAfgevinkteTaakToe(@PathParam("name")String name) {
+        alleTaken taak = alleTaken.getTaak();
+        List<Taak> taken = taak.getTaken();
+        List<Taak> afgevinkteTaken = taak.getAfgevinkteTaken();
+
+        for (Taak t : taken) {
+            if (t.getNaam().equals(name)) {
+                taken.remove(t);
+                afgevinkteTaken.add(t);
+                return Response.ok(t).build();
+            }
+        }
+        var error = new AbstractMap.SimpleEntry<>("error", "task not found!");
+        return Response.status(409).entity(error).build();
     }
 }
