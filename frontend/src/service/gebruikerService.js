1affc80f39784b63f0a72b074a964597cc107712
linde <linde.stam@student.hu.nl>
2024-06-24 19:07:40 +0200

    git ignore

--- frontend/src/service/gebruikerService.js
+++ frontend/src/service/gebruikerService.js
@@ -1,3 +1,45 @@
 function getGebruiker() {
+   return fetch("http://localhost:8080/restservices/gebruiker")
+       .then(response => {
+           return response.json().then(data => ({
+               status: response.status,
+               body: data
+           }));
+       })
+       .then(({ status, body }) => {
+           if (status === 200) {
+               console.log(body);
+               return body;
+           } else {
+               console.error(body.error);
+               return [];
+           }
+       })
+       .catch(error => {
+           console.error("Error fetching gebruikers:", error);
+           return [];
+       });
+}
+
+function addGebruiker(gebruiker) {
+    let url = "http://localhost:8080/restservices/addgebruiker"
+    let options = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(gebruiker),
+    };
+
+    return fetch(url, options)
+        .then(response => {
+            if(!response.ok) {
+                throw new Error("http error, : ${response.status}");
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error("error adding gebruikers", error);
+        })
 
 }
